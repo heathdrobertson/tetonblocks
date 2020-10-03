b19ab9a7f97a5867032b3aba110887cf39cf6a96
heathdrobertson <codehappens@toilethill.io>
2020-10-03 17:31:11 +0000

    React Site Setup

--- app/src/App.js
+++ app/src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState }from 'react';
+import { Container, Row, Col } from 'react-bootstrap'
+
+import Loading from './components/loading/Loading.js';
+import Dashboard from './components/dashboard/Dashboard.js';
+import Footer from './components/footer/Footer.js';
+
+import './assets/styles/App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        const [load, setLoad] = useState('false')
+
+        useEffect(() => {
+                //const timer = setTimeout(() => console.log("Hello, World!"), 3000);
+                const timer = setTimeout(() => setLoad('true'), 1200);
+                return () => clearTimeout(timer);
+        })
+        return (
+        <>
+        <div className="top-block">
+                {load === 'true' ? <Dashboard /> : <Loading /> }
+        </div>
+        <Footer />
+        </>
+        );
 }
 
 export default App;
