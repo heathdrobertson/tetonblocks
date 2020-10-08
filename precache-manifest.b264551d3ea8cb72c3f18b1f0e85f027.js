14f68367fcce59dfe3ba189f2c672149da21e3cf
heathdrobertson <codehappens@toilethill.io>
2020-10-08 05:03:07 +0000

    Updates

--- precache-manifest.b264551d3ea8cb72c3f18b1f0e85f027.js
+++ precache-manifest.b264551d3ea8cb72c3f18b1f0e85f027.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6ca82e14b0f012cea7582945c78cfdea",
+    "revision": "50e4a35616a370eefb8859d8a3344406",
     "url": "/index.html"
   },
   {
-    "revision": "c9bde645a2ba38b2783c",
+    "revision": "1570c587f5d6b596169c",
     "url": "/static/css/main.59ae2489.chunk.css"
   },
   {
-    "revision": "42d42f3e61fef0905c70",
-    "url": "/static/js/2.aae84431.chunk.js"
+    "revision": "1cdbbf59ef4f9e35b620",
+    "url": "/static/js/2.6b1b1ce4.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/static/js/2.aae84431.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.6b1b1ce4.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c9bde645a2ba38b2783c",
-    "url": "/static/js/main.afb377ec.chunk.js"
+    "revision": "1570c587f5d6b596169c",
+    "url": "/static/js/main.fae8da2c.chunk.js"
   },
   {
     "revision": "3bc6edfb4f4d8ff3ce81",
